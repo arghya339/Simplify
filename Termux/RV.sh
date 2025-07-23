e2a265ae0e9ef52f08bb4f78ba0a9c6c5343cad5
Arghyadeep Mondal <139842491+arghya339@users.noreply.github.com>
2025-07-23 14:22:02 +0530

    Test

--- Termux/RV.sh
+++ Termux/RV.sh
@@ -261,6 +261,7 @@ build_app() {
         local stock_apk_path=("$Download/${appNameRef[0]}_v${pkgVersion}-${archRef[0]}.apk")
       elif [ -z "$pkgVersion" ]; then
         local stock_apk=$(find "$Download" -type f -name "${appName}_v*-${arch}.apk" -print -quit)
+        echo -e "$notice DEBUG: appName: $appName, arch: $arch, stock_apk: ${stock_apk}"
         local stock_apk_path=("$stock_apk")
       fi
     fi
