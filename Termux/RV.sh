5ac63e353238f2f2d441fdae9d97c27e24743003
Arghyadeep Mondal <139842491+arghya339@users.noreply.github.com>
2025-07-25 22:23:37 +0530

    fix

--- Termux/RV.sh
+++ Termux/RV.sh
@@ -154,7 +154,7 @@ patch_app() {
     rm -rf "$without_ext-temporary-files"  # Remove temporary files directory
   else
     if [ "$appName" == "Instagram" ] || [ "$appName" == "Facebook" ] || [ "$appName" == "Facebook Messenger" ] || [ "$appName" == "Threads" ]; then
-      rm "$without_ext-options.json"
+      [[ -f "$without_ext-options.json" ]] && rm "$without_ext-options.json"
       rm "$without_ext.keystore"
     else
       rm "$without_ext.keystore"
