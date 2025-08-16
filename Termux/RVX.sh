0eda2a59b75c4ddf37ad7f686da13170cb33e309
Arghyadeep Mondal <139842491+arghya339@users.noreply.github.com>
2025-08-16 11:28:55 +0530

    fix

--- Termux/RVX.sh
+++ Termux/RVX.sh
@@ -220,7 +220,7 @@ yt_patches_args=(
 if su -c "id" >/dev/null 2>&1; then
   yt_patches_args+=(-d "GmsCore support")
 else
-  if [[ ( $Android -eq 7 || $Android -eq 6 ) && "$appName" == "YouTube" ]]; then
+  if [ $Android -eq 7 ] || [ $Android -eq 6 ]; then
     yt_patches_args+=(-e "GmsCore support" -OgmsCoreVendorGroupId="app.revanced" -OcheckGmsCore=true -OpackageNameYouTube="app.rvx.android.youtube")
   else
     yt_patches_args+=(-e "GmsCore support" -OgmsCoreVendorGroupId="com.mgoogle" -OcheckGmsCore=true -OpackageNameYouTube="app.rvx.android.youtube")
@@ -294,7 +294,7 @@ if [ "$ReadPatchesFile" -eq 1 ]; then
         if su -c "id" >/dev/null 2>&1; then
           echo "-d \"GmsCore support\"" >> "$SimplUsr/${arraynames[$i]}.txt"
         else
-          if [[ ( $Android -eq 7 || $Android -eq 6 ) && "$appName" == "YouTube" ]]; then
+          if [[ ( $Android -eq 7 || $Android -eq 6 ) && "${arraynames[$i]}" == "yt_patches_args" ]]; then
             echo "-e \"GmsCore support\" -O gmsCoreVendorGroupId=\"app.revanced\" -OcheckGmsCore=true" >> "$SimplUsr/${arraynames[$i]}.txt"
           else
             echo "-e \"GmsCore support\" -O gmsCoreVendorGroupId=\"com.mgoogle\" -OcheckGmsCore=true" >> "$SimplUsr/${arraynames[$i]}.txt"
