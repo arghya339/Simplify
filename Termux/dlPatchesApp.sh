2ca4a448dc1d3e61dc9f490527b16b1b8d3c36f7
Arghyadeep Mondal <139842491+arghya339@users.noreply.github.com>
2025-08-26 23:06:59 +0530

    test

--- Termux/dlPatchesApp.sh
+++ Termux/dlPatchesApp.sh
@@ -791,6 +791,7 @@ while true; do
       repo="Nagram"
       name=$(curl -s ${auth} "https://api.github.com/repos/$owner/$repo/releases/latest" | jq -r '.name')
       regex="Nagram-v${name}-${cpuAbi}.apk"
+      echo -e "$notice DEBUG - regex: $regex"
       file_pattern="Nagram-v${name}-${cpuAbi}.apk"
       tag=$(curl -s ${auth} "https://api.github.com/repos/$owner/$repo/releases/latest" | jq -r '.tag_name')
       assets="$regex"
