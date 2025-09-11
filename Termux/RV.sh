262eb02e3581e8c2b3328919b0c1b64388c49b6a
Arghyadeep Mondal <139842491+arghya339@users.noreply.github.com>
2025-09-11 19:14:46 +0530

    Disabled Spotify

--- Termux/RV.sh
+++ Termux/RV.sh
@@ -919,9 +919,9 @@ fi
 if  [[ $Android -ge 11  &&  ( "$cpuAbi" == "arm64-v8a" || "$cpuAbi" == "armeabi-v7a" ) ]]; then
   Facebook="Facebook"
 fi
-if su -c "id" >/dev/null 2>&1; then
-  Spotify="Spotify"
-fi
+#if su -c "id" >/dev/null 2>&1; then
+#  Spotify="Spotify"
+#fi
 
 # --- Arrays of apps list that required specific android version ---
 if [ $Android -ge 12 ]; then
