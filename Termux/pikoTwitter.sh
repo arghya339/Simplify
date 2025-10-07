8b8f54ca1338d42e5aff47a4e29973e6b9d924f5
Arghyadeep <139842491+arghya339@users.noreply.github.com>
2025-10-07 08:39:30 +0530

    fix

--- Termux/pikoTwitter.sh
+++ Termux/pikoTwitter.sh
@@ -63,45 +63,54 @@ fi
 
 echo -e "$info ${Blue}Target device:${Reset} $Model"
 
-# --- Download ReVanced CLI v3 ---
-ReVancedCLIJar="$pikoTwitter/revanced-cli-4.6.2-all.jar"
-if [ ! -f "$ReVancedCLIJar" ]; then
-  echo -e "$running Downloading revanced-cli-4.6.2-all.jar.."
-  url="https://github.com/inotia00/revanced-cli/releases/download/v4.6.2/revanced-cli-4.6.2-all.jar"
-  while true; do
-    #curl -L --progress-bar -C - -o "$ReVancedCLIJar" "$url"
-    aria2c -x 16 -s 16 --console-log-level=error --summary-interval=0 --download-result=hide -c -o "$(basename "$ReVancedCLIJar")" -d "$(dirname "$ReVancedCLIJar")" "$url"
-    if [ $? -eq 0 ]; then
-      echo  # White Space
-      break
-    fi
-    echo -e "${bad} ${Red}Download failed! retrying in 5 seconds..${Reset}"
-    sleep 5  # Wait 5 seconds
-  done
+if [ "$FetchPreRelease" -eq 0 ]; then
+  # --- Download ReVanced CLI v3 ---
+  ReVancedCLIJar="$pikoTwitter/revanced-cli-4.6.2-all.jar"
+  if [ ! -f "$ReVancedCLIJar" ]; then
+    echo -e "$running Downloading revanced-cli-4.6.2-all.jar.."
+    url="https://github.com/inotia00/revanced-cli/releases/download/v4.6.2/revanced-cli-4.6.2-all.jar"
+    while true; do
+      #curl -L --progress-bar -C - -o "$ReVancedCLIJar" "$url"
+      aria2c -x 16 -s 16 --console-log-level=error --summary-interval=0 --download-result=hide -c -o "$(basename "$ReVancedCLIJar")" -d "$(dirname "$ReVancedCLIJar")" "$url"
+      if [ $? -eq 0 ]; then
+        echo  # White Space
+        break
+      fi
+      echo -e "${bad} ${Red}Download failed! retrying in 5 seconds..${Reset}"
+      sleep 5  # Wait 5 seconds
+    done
+  fi
+  # --- Download ReVanced Integrations ---
+  bash $Simplify/dlGitHub.sh "crimera" "revanced-integrations" "$release" ".apk" "$pikoTwitter"
+  IntegrationsApk=$(find "$pikoTwitter" -type f -name "revanced-integrations-*.apk" -print -quit)
+  echo -e "$info ${Blue}IntegrationsApk:${Reset} $IntegrationsApk"
+else
+  # --- Download ReVanced CLI v5 ---
+  bash $Simplify/dlGitHub.sh "inotia00" "revanced-cli" "latest" ".jar" "$RVX"
+  ReVancedCLIJar=$(find "$RVX" -type f -name "revanced-cli-*-all.jar" -print -quit)
 fi
 echo -e "$info ${Blue}ReVancedCLIJar:${Reset} $ReVancedCLIJar"
 
 if [ "$FetchPreRelease" -eq 0 ]; then
   release="latest"  # Use latest release
+  fileType=".jar"
+  fileName="piko-twitter-patches-*.jar"
   tag=$(curl -sL ${auth} "https://api.github.com/repos/crimera/piko/releases/latest" | jq -r '.tag_name')
 else
   release="pre"  # Use pre-release
+  fileType=".rvp"
+  fileName="patches-*.rvp"
   tag=$(curl -sL ${auth} "https://api.github.com/repos/crimera/piko/releases" | jq -r '.[].tag_name | select(contains("dev"))' | head -n 1)
 fi
 
 # --- Download piko Patches ---
-bash $Simplify/dlGitHub.sh "crimera" "piko" "$release" ".jar" "$pikoTwitter"
-PatchesJar=$(find "$pikoTwitter" -type f -name "piko-twitter-patches-*.jar" -print -quit)
-echo -e "$info ${Blue}PatchesJar:${Reset} $PatchesJar"
+bash $Simplify/dlGitHub.sh "crimera" "piko" "$release" "$fileType" "$pikoTwitter"
+PatchesRvp=$(find "$pikoTwitter" -type f -name "$fileName" -print -quit)
+echo -e "$info ${Blue}PatchesRvp:${Reset} $PatchesRvp"
 #echo -e "${Cyan}~~~~~~~~~~~~~~~~~~~~~release notes~~~~~~~~~~~~~~~~~~~~~~${Reset}"
 echo -e "${Cyan}~~~~~~~~~~~~~~~~~~~~~~~changelog~~~~~~~~~~~~~~~~~~~~~~~~${Reset}"
 curl -sL ${auth} "https://api.github.com/repos/crimera/piko/releases/tags/$tag" | jq -r .body | glow  # Display the release notes
 echo -e "${Cyan}~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~${Reset}"
-
-# --- Download ReVanced Integrations ---
-bash $Simplify/dlGitHub.sh "crimera" "revanced-integrations" "$release" ".apk" "$pikoTwitter"
-IntegrationsApk=$(find "$pikoTwitter" -type f -name "revanced-integrations-*.apk" -print -quit)
-echo -e "$info ${Blue}IntegrationsApk:${Reset} $IntegrationsApk"
 
 # --- Generate ripLib arg ---
 if [ "$RipLib" -eq 1 ]; then
@@ -130,8 +139,8 @@ getVersion() {
   local pkgName="$1"
   
   # Get all versions for the package and sort them, then take the highest version
-  pkgVersion=$($PREFIX/lib/jvm/java-$jdkVersion-openjdk/bin/java -jar $ReVancedCLIJar list-versions $PatchesJar -f=$pkgName | sed 's/^[[:space:]]*//; s/ (.*//;' | grep -E '^[0-9]|^Any$' | sort -rV | head -n 2 | head -n 1)
-  preVersion=$($PREFIX/lib/jvm/java-$jdkVersion-openjdk/bin/java -jar $ReVancedCLIJar list-versions $PatchesJar -f=$pkgName | sed 's/^[[:space:]]*//; s/ (.*//;' | grep -E '^[0-9]|^Any$' | sort -rV | head -n 2 | tail -n 1)
+  pkgVersion=$($PREFIX/lib/jvm/java-$jdkVersion-openjdk/bin/java -jar $ReVancedCLIJar list-versions $PatchesRvp -f=$pkgName | sed 's/^[[:space:]]*//; s/ (.*//;' | grep -E '^[0-9]|^Any$' | sort -rV | head -n 2 | head -n 1)
+  preVersion=$($PREFIX/lib/jvm/java-$jdkVersion-openjdk/bin/java -jar $ReVancedCLIJar list-versions $PatchesRvp -f=$pkgName | sed 's/^[[:space:]]*//; s/ (.*//;' | grep -E '^[0-9]|^Any$' | sort -rV | head -n 2 | tail -n 1)
   if [ -z "$pkgVersion" ] || [ "$pkgVersion" == "Any" ] || [ "$pkgVersion" == "null" ]; then
     if [ "$release" == "pre" ]; then
       pkgVersion=$(curl -sL ${auth} "https://api.github.com/repos/crimera/twitter-apk/releases" | jq -r '.[].tag_name' | head -1)  # Last Releases
@@ -140,8 +149,8 @@ getVersion() {
     fi
     preVersion=$(curl -sL ${auth} "https://api.github.com/repos/crimera/twitter-apk/releases" | jq -r '.[].tag_name' | head -n 2 | tail -n 1)  # Previous Releases
     if [ -z "$pkgVersion" ]; then
-      pkgVersion=$($PREFIX/lib/jvm/java-$jdkVersion-openjdk/bin/java -jar $ReVancedCLIJar list-patches -d=true -f=$pkgName -i=false -o=false -p=false -u -v=true $PatchesJar | grep -oP 'Requires X \K[\d.]+-release\.\d+' | sort -rV | head -n 1)
-      preVersion=$($PREFIX/lib/jvm/java-$jdkVersion-openjdk/bin/java -jar $ReVancedCLIJar list-patches -d=true -f=$pkgName -i=false -o=false -p=false -u -v=true $PatchesJar | grep -oP 'Requires X \K[\d.]+-release\.\d+' | sort -rV | head -n 2 | tail -n 1)
+      pkgVersion=$($PREFIX/lib/jvm/java-$jdkVersion-openjdk/bin/java -jar $ReVancedCLIJar list-patches -d=true -f=$pkgName -i=false -o=false -p=false -u -v=true $PatchesRvp | grep -oP 'Requires X \K[\d.]+-release\.\d+' | sort -rV | head -n 1)
+      preVersion=$($PREFIX/lib/jvm/java-$jdkVersion-openjdk/bin/java -jar $ReVancedCLIJar list-patches -d=true -f=$pkgName -i=false -o=false -p=false -u -v=true $PatchesRvp | grep -oP 'Requires X \K[\d.]+-release\.\d+' | sort -rV | head -n 2 | tail -n 1)
     fi
   fi
 
@@ -156,10 +165,17 @@ patch_twitter() {
   without_ext="${outputAPK%.*}"  # remove file extension (.apk)
   local log="$SimplUsr/piko-twitter_patch-log.txt"
   
-  $PREFIX/lib/jvm/java-$jdkVersion-openjdk/bin/java -jar $ReVancedCLIJar patch -b $PatchesJar -m $IntegrationsApk \
-    -o "$outputAPK" "${stock_apk_ref[0]}" \
-    -i "Bring back twitter" -i "Enable app downgrading" -e "Export all activities" \
-    --custom-aapt2-binary="$HOME/aapt2" --purge $ripLib -f | tee "$log"
+  if [ "$FetchPreRelease" -eq 0 ]; then
+    $PREFIX/lib/jvm/java-$jdkVersion-openjdk/bin/java -jar $ReVancedCLIJar patch -b $PatchesRvp -m $IntegrationsApk \
+      -o "$outputAPK" "${stock_apk_ref[0]}" \
+      -i "Bring back twitter" -i "Enable app downgrading" -e "Export all activities" \
+      --custom-aapt2-binary="$HOME/aapt2" --purge $ripLib -f | tee "$log"
+  else
+    $PREFIX/lib/jvm/java-$jdkVersion-openjdk/bin/java -jar $ReVancedCLIJar patch -p $PatchesRvp \
+      -o "$outputAPK" "${stock_apk_ref}" \
+      -e "Bring back twitter" -e "Enable app downgrading" -d "Export all activities" \
+      --custom-aapt2-binary="$HOME/aapt2" --purge $ripLib -f | tee "$log"
+  fi
   
   if [ ! -f "$outputAPK" ] && [ -f "${stock_apk_ref[0]}" ]; then
     echo -e "$bad Oops, Piko Twitter Patching failed !! Logs saved to "$log". Share the Patchlog to developer."
