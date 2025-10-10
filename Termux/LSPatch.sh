ae7721b46e32f893dffecb9fa12e7c5b7f2e230d
Arghyadeep <139842491+arghya339@users.noreply.github.com>
2025-10-10 21:35:39 +0530

    fix

--- Termux/LSPatch.sh
+++ Termux/LSPatch.sh
@@ -156,6 +156,53 @@ patch_app() {
   fi
 }
 
+confirmPrompt() {
+  Prompt=${1}
+  local -n prompt_buttons=$2
+  Selected=${3:-0}  # :- set value as 0 if unset
+  maxLen=50
+  
+  # breaks long prompts into multiple lines (50 characters per line)
+  lines=()  # empty array
+  while [ -n "$Prompt" ]; do
+    lines+=("${Prompt:0:$maxLen}")  # take first 50 characters from $Prompt starting at index 0
+    Prompt="${Prompt:$maxLen}"  # removes first 50 characters from $Prompt by starting at 50 to 0
+  done
+  
+  # print all-lines except last-line
+  last_line_index=$(( ${#lines[@]} - 1 ))  # ${#lines[@]} = number of elements in lines array
+  for (( i=0; i<last_line_index; i++ )); do
+    echo -e "${lines[i]}"
+  done
+  last_line="${lines[$last_line_index]}"
+  
+  echo -ne '\033[?25l'  # Hide cursor
+  while true; do
+    show_prompt() {
+      echo -ne "\r\033[K"  # n=noNewLine r=returnCursorToStartOfLine \033[K=clearLine
+      echo -ne "$last_line "
+      [ $Selected -eq 0 ] && echo -ne "${whiteBG}➤ ${prompt_buttons[0]} $Reset   ${prompt_buttons[1]}" || echo -ne "  ${prompt_buttons[0]}  ${whiteBG}➤ ${prompt_buttons[1]} $Reset"  # highlight selected bt with white bg
+    }; show_prompt
+
+    read -rsn1 key
+    case $key in
+      $'\E')
+      # /bin/bash -c 'read -r -p "Type any ESC key: " input && printf "You Entered: %q\n" "$input"'  # q=safelyQuoted
+        read -rsn2 -t 0.1 key2  # -r=readRawInput -s=silent(noOutput) -t=timeout -n2=readTwoChar | waits upto 0.1s=100ms to read key 
+        case $key2 in 
+          '[C') Selected=1 ;;  # right arrow key
+          '[D') Selected=0 ;;  # left arrow key
+        esac
+        ;;
+      [Yy]*) Selected=0; show_prompt; break ;;
+      [Nn]*) Selected=1; show_prompt; break ;;
+      "") break ;;  # Enter key
+    esac
+  done
+  echo -e '\033[?25h' # Show cursor
+  return $Selected  # return Selected int index from this fun
+}
+
 # --- function to Build App ---
 build_app() {
   # local variables
@@ -276,17 +323,16 @@ build_app() {
     
     else
       
-      echo -e "[?] ${Yellow}Do you want to Install ${appNameRef[0]} LSPatch app? [Y/n] ${Reset}\c" && read opt
+      buttons=("<Yes>" "<No>"); confirmPrompt "Do you want to Install ${appNameRef[0]} LSPatch app?" "buttons" && opt=Yes || opt=No
       case $opt in
         y*|Y*|"")
           echo -e "$running Please Wait !! Installing Patched ${appNameRef[0]} LSPatch apk.."
           bash $Simplify/apkInstall.sh "${output_apk_path}" "$activityPatched"
           ;;
         n*|N*) echo -e "$notice ${appNameRef[0]} LSPatch Installaion skipped!" ;;
-        *) echo -e "$info Invalid choice! ${appNameRef[0]} LSPatch Installaion skipped." ;;
       esac
       
-      echo -e "[?] ${Yellow}Do you want to Share ${appNameRef[0]} LSPatch app? [Y/n] ${Reset}\c" && read opt
+      buttons=("<Yes>" "<No>"); confirmPrompt "Do you want to Share ${appNameRef[0]} LSPatch app?" "buttons" "1" && opt=Yes || opt=No
       case $opt in
         y*|Y*|"")
           echo -e "$running Please Wait !! Sharing Patched ${appNameRef[0]} LSPatch apk.."
@@ -299,7 +345,6 @@ build_app() {
             am start -a android.intent.action.VIEW -d "content://com.android.externalstorage.documents/document/primary:Simplify" -t "vnd.android.document/directory" -n com.android.documentsui/com.android.documentsui.files.FilesActivity > /dev/null 2>&1  # Open Android Files
           fi
           ;;
-        *) echo -e "$info Invalid choice! ${appNameRef[0]} LSPatch Sharing skipped." ;;
       esac
 
     fi
@@ -369,17 +414,16 @@ sign_app() {
   
   if [ -f "${output_apk_path}" ]; then
     
-    echo -e "[?] ${Yellow}Do you want to Install ${appNameRef[0]} Signed app? [Y/n] ${Reset}\c" && read opt
+    buttons=("<Yes>" "<No>"); confirmPrompt "Do you want to Install ${appNameRef[0]} Signed app?" "buttons" && opt=Yes || opt=No
     case $opt in
       y*|Y*|"")
         echo -e "$running Please Wait !! Installing Signed ${appNameRef[0]} apk.."
         bash $Simplify/apkInstall.sh "${output_apk_path}" "$activityPatched"
         ;;
       n*|N*) echo -e "$notice ${appNameRef[0]} Signed Installaion skipped!" ;;
-      *) echo -e "$info Invalid choice! ${appNameRef[0]} Signed Installaion skipped." ;;
     esac
       
-    echo -e "[?] ${Yellow}Do you want to Share ${appNameRef[0]} Signed app? [Y/n] ${Reset}\c" && read opt
+    buttons=("<Yes>" "<No>"); confirmPrompt "Do you want to Share ${appNameRef[0]} Signed app?" "buttons" "1" && opt=Yes || opt=No
     case $opt in
       y*|Y*|"")
         echo -e "$running Please Wait !! Sharing Signed ${appNameRef[0]} apk.."
@@ -392,7 +436,6 @@ sign_app() {
           am start -a android.intent.action.VIEW -d "content://com.android.externalstorage.documents/document/primary:Simplify" -t "vnd.android.document/directory" -n com.android.documentsui/com.android.documentsui.files.FilesActivity > /dev/null 2>&1  # Open Android Files
         fi
         ;;
-      *) echo -e "$info Invalid choice! ${appNameRef[0]} Signed Sharing skipped." ;;
     esac
     
   fi
