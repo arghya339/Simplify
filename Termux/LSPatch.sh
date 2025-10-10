479fcfb7c16c30f88c6a89b4c7a2075c470f4cd0
Arghyadeep <139842491+arghya339@users.noreply.github.com>
2025-10-10 22:25:07 +0530

    forgot

--- Termux/LSPatch.sh
+++ Termux/LSPatch.sh
@@ -185,7 +185,7 @@ confirmPrompt() {
         [ $Selected -eq 0 ] && echo -ne "${whiteBG}➤ ${prompt_buttons[0]} $Reset   ${prompt_buttons[1]}" || echo -ne "  ${prompt_buttons[0]}  ${whiteBG}➤ ${prompt_buttons[1]} $Reset"  # highlight selected bt with white bg
       elif [ ${#prompt_buttons[@]} -eq 3 ]; then
         if [ $Selected -eq 0 ]; then
-          echo -ne "${whiteBG}➤ ${prompt_buttons[0]} $Reset   ${prompt_buttons[1]}"
+          echo -ne "${whiteBG}➤ ${prompt_buttons[0]} $Reset   ${prompt_buttons[1]}   ${prompt_buttons[2]}"
         elif [ $Selected -eq 1 ]; then
           echo -ne "  ${prompt_buttons[0]}  ${whiteBG}➤ ${prompt_buttons[1]} $Reset  ${prompt_buttons[2]}"
         elif [ $Selected -eq 2 ]; then
