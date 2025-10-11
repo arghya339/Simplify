fea1c58621e9f29068b7a2a497d79620bc043cd0
Arghyadeep <139842491+arghya339@users.noreply.github.com>
2025-10-11 21:04:57 +0530

    test

--- Termux/dlPatchedApp.sh
+++ Termux/dlPatchedApp.sh
@@ -380,7 +380,6 @@ fi
 # --- Arrays of apps list that required specific android version ---
 if [ $Android -ge 10 ]; then
   apps=(
-    Quit
     Vanced\ MicroG
     ReVanced\ GmsCore
     YouTube\ RV
@@ -428,7 +427,6 @@ if [ $Android -ge 10 ]; then
   )
 elif [ $Android -eq 9 ]; then
   apps=(
-    Quit
     Vanced\ MicroG
     ReVanced\ GmsCore
     YouTube\ RV
@@ -475,7 +473,6 @@ elif [ $Android -eq 9 ]; then
   )
 elif [ $Android -eq 8 ]; then
   apps=(
-    Quit
     Vanced\ MicroG
     ReVanced\ GmsCore
     YouTube\ RV
@@ -520,7 +517,6 @@ elif [ $Android -eq 8 ]; then
   )
 elif [ $Android -eq 7 ]; then
   apps=(
-    Quit
     Vanced\ MicroG
     ReVanced\ GmsCore
     YouTube
@@ -553,7 +549,6 @@ elif [ $Android -eq 7 ]; then
   )
 elif [ $Android -eq 6 ]; then
   apps=(
-    Quit
     Vanced\ MicroG
     ReVanced\ GmsCore
     YouTube
@@ -578,7 +573,6 @@ elif [ $Android -eq 6 ]; then
   )
 elif [ $Android -eq 5 ]; then
   apps=(
-    Quit
     YTPro
     Tubular
     Vanced\ MicroG
@@ -602,42 +596,133 @@ elif [ $Android -eq 5 ]; then
   )
 elif [ $Android -eq 4 ]; then
   apps=(
-    Quit
     ReVanced\ GmsCore
     RAR
     Solid\ Explorer
   )
 fi
 
-while true; do
-  # Display the apps list
-  echo -e "$info Available apps:"
-  for i in "${!apps[@]}"; do
-    if [ -n "${apps[$i]}" ] && [ "${apps[$i]}" != "null" ]; then
-      if [ "$i" -le 9 ]; then
-        printf "%d . %s\n" "$i" "${apps[$i]}"
+menu() {
+  local -n menu_options=$1
+  local -n menu_buttons=$2
+  items_per_page=${3:-12}  # Default to 12 if items/page not provided
+  
+  selected_option=0
+  selected_button=0
+  
+  current_page=0
+  total_pages=$(( (${#menu_options[@]} + items_per_page - 1) / items_per_page ))  # Convert to integer from floating point page number
+
+  show_menu() {
+    printf '\033[2J\033[3J\033[H'
+    # Display guide
+    echo -n "Navigate with [↑] [↓] [←] [→]"
+    [ $total_pages -gt 1 ] && echo -n " [PGUP] [PGDN]"
+    echo -e "\nSelect with [↵]\n"
+    
+    # Calculate start and end indices for current page
+    start_index=$(( current_page * items_per_page ))
+    end_index=$(( start_index + (items_per_page - 1) ))
+    [ $end_index -ge ${#menu_options[@]} ] && end_index=$((${#menu_options[@]} - 1))
+    
+    # Display menu options for current page
+    for ((i=start_index; i<=end_index; i++)); do
+      if [ $i -eq $selected_option ]; then
+        echo -e "${whiteBG}➤ ${menu_options[$i]} $Reset"
       else
-        printf "%d. %s\n" "$i" "${apps[$i]}"
+        [ $(($i + 1)) -le 9 ] && echo " $(($i + 1)). ${menu_options[$i]}" || echo "$(($i + 1)). ${menu_options[$i]}"
       fi
-    fi
+    done
+    
+    for ((i=end_index+1; i < start_index + items_per_page; i++)); do echo; done  # Fill remaining lines if current page has fewer than items/page options
+    
+    [ $total_pages -gt 1 ] && echo -e "\nPage: $((current_page + 1))/$total_pages\n" || echo  # Display page info if multiple pages exist
+    
+    # Display buttons
+    for ((i=0; i<=$((${#menu_buttons[@]} - 1)); i++)); do
+      if [ $i -eq $selected_button ]; then
+        [ $i -eq 0 ] && echo -ne "${whiteBG}➤ ${menu_buttons[$i]} $Reset" || echo -ne "  ${whiteBG}➤ ${menu_buttons[$i]} $Reset"
+      else
+        [ $i -eq 0 ] && echo -n "  ${menu_buttons[$i]}" || echo -n "   ${menu_buttons[$i]}"
+      fi
+    done
+  }
+
+  printf '\033[?25l'
+  while true; do
+    show_menu
+    read -rsn1 key
+    case $key in
+      $'\E')  # ESC
+        # /bin/bash -c 'read -r -p "Type any ESC key: " input && printf "You Entered: %q\n" "$input"'  # q=safelyQuoted
+        read -rsn2 -t 0.1 key2
+        case "$key2" in
+          '[A')  # Up arrow
+            selected_option=$((selected_option - 1))
+            [ $selected_option -lt 0 ] && selected_option=$((${#menu_options[@]} - 1))
+            current_page=$((selected_option / items_per_page))  # Auto switch page
+            ;;
+          '[B')  # Down arrow
+            selected_option=$((selected_option + 1))
+            [ $selected_option -ge ${#menu_options[@]} ] && selected_option=0
+            current_page=$((selected_option / items_per_page))  # Auto switch page
+            ;;
+          '[C')  # Right arrow
+            [ $selected_button -lt $((${#menu_buttons[@]} - 1)) ] && selected_button=$((selected_button + 1))
+            ;;
+          '[D')  # Left arrow
+            [ $selected_button -gt 0 ] && selected_button=$((selected_button - 1))
+            ;;
+          '[5') # Page Up
+            read -rsn1 -t 0.1 key3
+            if [ "$key3" == "~" ]; then
+              current_page=$((current_page - 1))
+              [ $current_page -lt 0 ] && current_page=$((total_pages - 1))
+              selected_option=$((current_page * items_per_page))  # Update selected option to start indices on new page
+            fi
+            ;;
+          '[6') # Page Down
+            read -rsn1 -t 0.1 key3
+            if [ "$key3" == "~" ]; then
+              current_page=$((current_page + 1))
+              [ $current_page -ge $total_pages ] && current_page=0
+              selected_option=$((current_page * items_per_page))  # Update selected option to start indices on new page
+            fi
+            ;;
+        esac
+        ;;
+      '')  # Enter key
+        break
+        ;;
+      [0-9])
+        read -rsn2 -t0.5 key2
+        [[ "$key2" == [0-9] ]] && { key="${key}${key2}"; key=$((10#$key)); }  # Convert to integer (decimal) from strings
+        if [ $key -eq 0 ]; then
+          selected_option=$((${#menu_options[@]} - 1))
+        elif [ $key -gt ${#menu_options[@]} ]; then
+          selected_option=0
+        else
+          selected_option=$(($key - 1))
+        fi
+        current_page=$((selected_option / items_per_page))  # Auto switch page
+        show_menu; sleep 0.5; break
+       ;;
+    esac
   done
+  printf '\033[?25h'
 
-  # Ask for an index, showing the valid range
-  max=$(( ${#apps[@]} - 1 ))  # highest legal index
-  read -rp "Enter the index [0-${max}] of the apps you want to download: " idx
-
-  # Validate and respond
-  if [ $idx == 0 ]; then
-    break  # break the while loop
-  elif [[ $idx =~ ^[0-9]+$ ]] && (( idx >= 0 && idx <= max )); then
-    echo -e "$notice Selected: ${apps[$idx]}"
-  else
-    echo -e "$info \"$idx\" is not a valid index! Please select index [0-${max}]." >&2
+  [ $selected_button -eq 0 ] && { printf '\033[2J\033[3J\033[H'; selected=$selected_option; }
+  if [ $selected_button -eq $((${#menu_buttons[@]} - 1)) ]; then
+    [ "${menu_buttons[$((${#menu_buttons[@]} - 1))]}" == "<Back>" ] && { printf '\033[2J\033[3J\033[H'; return 1; } || { [ $isOverwriteTermuxProp -eq 1 ] && sed -i '/allow-external-apps/s/^/# /' "$HOME/.termux/termux.properties"; printf '\033[2J\033[3J\033[H'; echo "Script exited !!"; exit 0; }
   fi
+}
+
+while true; do
+  buttons=("<Select>" "<Back>"); if menu "options" "buttons" "22"; then selected="${options[$selected]}"; else break; fi
   
   release=latest
   # main conditional control flow
-  case ${apps[$idx]} in
+  case "$selected" in
     Vanced\ MicroG)
       appName="Vanced MicroG"
       repo="VancedMicroG"
