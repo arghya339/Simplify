aa3b882930df0879ebf0c79f6c6e050a64c293fb
Arghyadeep <139842491+arghya339@users.noreply.github.com>
2025-10-17 00:58:46 +0530

    Add: Phone by Google v195.0.815454329 support

--- Termux/LSPatch.sh
+++ Termux/LSPatch.sh
@@ -408,7 +408,7 @@ while true; do
       appName=("Phone by Google")
       pkgName="com.google.android.dialer"
       if [ $Android -ge 11 ]; then
-        pkgVersion="194.0.813798200"
+        pkgVersion="195.0.815454329"
         #pkgVersion=""
       elif [ $Android -eq 10 ]; then
         pkgVersion="161.0.726587057"
