64f7a0ce65fd6c696939596703c493cd59a2f884
Arghyadeep <139842491+arghya339@users.noreply.github.com>
2025-10-27 18:54:59 +0530

    forgot

--- Termux/dlPatchedApp.sh
+++ Termux/dlPatchedApp.sh
@@ -712,6 +712,7 @@ while true; do
             file_pattern="${appName}_v$tag-noarch.apk"
             apk_path=$(find "$Download" -type f -name "$file_pattern" -print -quit)
             version=$($HOME/aapt2 dump badging $apk_path 2>/dev/null | sed -n "s/.*versionName='\([^']*\)'.*/\1/p")
+            url="https://www.apkmirror.com/apk/xiaomi-inc/miui-compass/xiaomi-compass-16-0-6-0-release/xiaomi-compass-16-0-6-0-android-apk-download/"
             appInstall
             ;;
           Google\ TV\ →\ CloudStream)
