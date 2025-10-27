21eea8a2181db0f712bd9c881010ed9c675dff34
Arghyadeep <139842491+arghya339@users.noreply.github.com>
2025-10-27 09:10:49 +0530

    mistake

--- Termux/Simplify.sh
+++ Termux/Simplify.sh
@@ -946,7 +946,7 @@ overwriteArch() {
     echo -e "$info Device architecture not spoofed yet!"
   fi
   sleep 1
-  options=(Disabled\ Arch\ spoofing arm64-v8a armeabi-v7a x86_64 x86); buttons=("<Select>" "<Back>"); if menu "options" "buttons" "5"; then arch="${options[$selected]}"; fi
+  opt=(Disabled\ Arch\ spoofing arm64-v8a armeabi-v7a x86_64 x86); buttons=("<Select>" "<Back>"); if menu "opt" "buttons" "5"; then arch="${opt[$selected]}"; fi
   if [ -n "$arch" ]; then
     case "$arch" in
       Disabled\ Arch\ spoofing)
