d266cbb6c871623259bcc0eb90fe2e3aecf702b0
Arghyadeep <139842491+arghya339@users.noreply.github.com>
2025-10-27 12:30:21 +0530

    fix

--- Termux/dlPatchedApp.sh
+++ Termux/dlPatchedApp.sh
@@ -685,11 +685,11 @@ while true; do
             ghApiResponseJson=$(curl -s ${auth} "https://api.github.com/repos/$owner/$repo/releases/latest")
             tag=$(jq -r '.tag_name' <<< "$ghApiResponseJson")
             name=$(jq -r '.name' <<< "$ghApiResponseJson" | sed 's/ Release$//')
-            regex="$repo-$name-$cpuAbi.apk"
-            echo -e "$notice DEBUG - regex: $regex" && sleep 30
+            regex="$repo-$name*-$cpuAbi.apk"
+            assets=$(jq -r --arg regex "$regex" '.assets[] | select(.name | test($regex)) | .name' <<< "$ghApiResponseJson" | head -1)
             pkgApp="com.dot.gallery"
             activityApp="com.dot.gallery/.feature_node.presentation.main.MainActivity"
-            dlApp "${appName}" "$owner" "$repo" "$release" "$regex" "$file_pattern" "$tag" "$regex" "$pkgApp" "$activityApp"
+            dlApp "${appName}" "$owner" "$repo" "$release" "$assets" "$file_pattern" "$tag" "$assets" "$pkgApp" "$activityApp"
             ;;
           Chrome\ →\ Chromium) termux-open-url "https://github.com/arghya339/crdl" ;;
           Chrome\ →\ Firefox) termux-open-url "https://play.google.com/store/apps/details?id=org.mozilla.firefox" ;;
