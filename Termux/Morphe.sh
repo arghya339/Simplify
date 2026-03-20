e75430ef49198e7df909c9cdac6709d7d5708424
Arghyadeep Mondal <arghyadeepmondal339@duck.com>
2026-03-20 16:44:01 +0530

    Add: YouTube v21.12.521 support

--- Termux/Morphe.sh
+++ Termux/Morphe.sh
@@ -310,7 +310,7 @@ while true; do
       pkgName="com.google.android.youtube"
       appName=("YouTube")
       [ $Android -eq 8 ] && pkgVersion="20.26.46" || pkgVersion=""
-      ([ $Android -ge 9 ] && [ $FetchPreRelease -eq 1 ]) && pkgVersion="21.11.480"
+      ([ $Android -ge 9 ] && [ $FetchPreRelease -eq 1 ]) && pkgVersion="21.12.521"
       if [ -z "$pkgVersion" ]; then
         getVersion "$pkgName"
         pkgVersion="$pkgVersion"
