a50db2ee034a60c4869615fdf67e6f5368f13c85
Arghyadeep Mondal <arghyadeepmondal339@duck.com>
2026-05-03 14:03:54 +0530

    Add: YT Music v9.17.51 && Reddit v2026.18.0 support

--- Termux/Morphe.sh
+++ Termux/Morphe.sh
@@ -325,7 +325,7 @@ while true; do
       pkgName="com.google.android.apps.youtube.music"
       appName=("YouTube Music")
       pkgVersion=""
-      [ $FetchPreRelease -eq 1 ] && pkgVersion="9.16.51"
+      [ $FetchPreRelease -eq 1 ] && pkgVersion="9.17.51"
       if [ -z "$pkgVersion" ]; then
         getVersion "$pkgName"
         pkgVersion="$pkgVersion"
@@ -340,7 +340,7 @@ while true; do
       pkgName="com.reddit.frontpage"
       appName=("Reddit")
       [ $Android -eq 9 ] && pkgVersion="2026.10.0" || pkgVersion=""
-      ([ $Android -gt 9 ] && [ $FetchPreRelease -eq 1 ]) && pkgVersion="2026.17.0"
+      ([ $Android -gt 9 ] && [ $FetchPreRelease -eq 1 ]) && pkgVersion="2026.18.0"
       if [ -z "$pkgVersion" ]; then
         getVersion "$pkgName"
         pkgVersion="$pkgVersion"
