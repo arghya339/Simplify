b75ff3a115eba4ded17ceaf0c44757f597a152a5
Arghyadeep Mondal <arghyadeepmondal339@duck.com>
2026-08-18 14:09:23 +0530

    Add: YT Music v9.32.51 support

--- Termux/Morphe.sh
+++ Termux/Morphe.sh
@@ -324,7 +324,7 @@ while true; do
       pkgName="com.google.android.apps.youtube.music"
       appName=("YouTube Music")
       pkgVersion=""
-      [ $FetchPreRelease -eq 1 ] && pkgVersion="9.31.51"
+      [ $FetchPreRelease -eq 1 ] && pkgVersion="9.32.51"
       if [ -z "$pkgVersion" ]; then
         getVersion "$pkgName"
         pkgVersion="$pkgVersion"
