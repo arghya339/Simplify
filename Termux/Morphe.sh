6779664aac717a8c28bd697b7bb1afac630ec55e
Arghyadeep Mondal <arghyadeepmondal339@duck.com>
2026-09-19 21:07:15 +0530

    Add: Reddit v2026.38.0 support

--- Termux/Morphe.sh
+++ Termux/Morphe.sh
@@ -339,7 +339,7 @@ while true; do
       pkgName="com.reddit.frontpage"
       appName=("Reddit")
       [ $Android -eq 9 ] && pkgVersion="2026.10.0" || pkgVersion=""
-      ([ $Android -gt 9 ] && [ $FetchPreRelease -eq 1 ]) && pkgVersion="2026.37.0"
+      ([ $Android -gt 9 ] && [ $FetchPreRelease -eq 1 ]) && pkgVersion="2026.38.0"
       if [ -z "$pkgVersion" ]; then
         getVersion "$pkgName"
         pkgVersion="$pkgVersion"
